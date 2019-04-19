c33c42eaa87d15b736ae765a38679c8ce58eb127
Joseph Laquinte <joseph.laquinte@ogilvy.com>
2019-04-19 12:31:38 -0400

    removing create react app scaffolding and adding basic project structure

---
+++ src/components/DeliveryEvent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class DeliveryEvent extends Component {
+    constructor(){
+        super()
+
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+        	<h1>Delivery Event</h1>
+        );
+    }
+}
+
+
+export default DeliveryEvent
---
+++ src/components/InventoryFilter.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import DeliveryEvent from './DeliveryEvent'
+
+class InventoryFilter extends Component {
+    constructor(){
+        super()
+
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+        	<DeliveryEvent />
+        );
+    }
+}
+
+
+export default InventoryFilter
+
