e8198fcd101ccd12d6cf5f00314e0e1bec730722
Joseph Laquinte <joseph.laquinte@ogilvy.com>
2019-04-19 15:13:30 -0400

    passing event names into input

--- src/components/DeliveryEvent.js
+++ src/components/DeliveryEvent.js
@@ -4,13 +4,22 @@ class DeliveryEvent extends Component {
     constructor(props){
         super(props)
 
-        this.state = {}
+        this.state = {
+        	isSelected: false
+        }
+    }
+
+    toggleChecked(){
+    	this.setState({ isSelected: !this.state.isSelected})
     }
 
     render() {
         return (
         	<li className="delivery-event">
-        		<input type="checkbox" />
+        		<input type="checkbox" onClick={ ()=>{ 
+        			this.toggleChecked()
+        			this.props.onSelected(this.props.name, !this.state.isSelected)} 
+        		}/>
         		<span className="event-color-tag" style={{backgroundColor:this.props.tagColor}}></span>
         		<p className="event-name">{this.props.name}</p>
         	</li>
--- src/components/InventoryFilter.js
+++ src/components/InventoryFilter.js
@@ -3,31 +3,54 @@ import DeliveryEvent from './DeliveryEvent'
 import data from '../api/data'
 
 class InventoryFilter extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
 
         this.state = {
-        	data
+        	data,
+        	selectedEvents: []
         }
     }
+
+    filterItems(selectedItem, isSelected){
+    	let itemList = this.state.selectedEvents
+
+    	if(isSelected)
+    		itemList.push(selectedItem)
+    	else
+    		itemList.splice(itemList.indexOf(selectedItem),1)
+
+    	this.setState({selectedEvents: itemList})
+    }
+
 
     render() {
         return (
         	<section className="delivery-event-filter">
-	        	<input type="text" className="active-delivery-events" size="75"/>
+	        	<input 
+	        		type="text" 
+	        		onChange= {()=>{}}
+	        		value= {this.state.selectedEvents} 
+	        		className= "active-delivery-events" 
+	        		size="75" 
+	        		placeholder="Select..."
+	        	/>
 				<ul className="delivery-event-list">
 					{data.map(entry =>
 						<DeliveryEvent
 							key={entry.id}
+							id={entry.id}
 	        				name={entry.name}
 	        				tagColor={entry.hexColor}
+	        				onSelected={ (selectedEvent, isSelected) =>{
+	        					this.filterItems(selectedEvent, isSelected)
+	        				}}
 	        			/>
 					)}
 				</ul>
-			</section>	
+			</section>
         );
     }
 }
-
 
 export default InventoryFilter
